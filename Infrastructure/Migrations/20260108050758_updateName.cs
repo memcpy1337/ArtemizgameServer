dd25de30f2b95f9e72c1f8cccc71162214792cd7
DESKTOP-0D5SAAB\artem <artemizgame@gmail.com>
2026-01-08 08:08:14 +0300

    1

---
+++ Infrastructure/Migrations/20260108050758_updateName.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GameVersion",
+                table: "GameVersion");
+
+            migrationBuilder.RenameTable(
+                name: "GameVersion",
+                newName: "GameVersions");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GameVersions",
+                table: "GameVersions",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_GameVersions",
+                table: "GameVersions");
+
+            migrationBuilder.RenameTable(
+                name: "GameVersions",
+                newName: "GameVersion");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_GameVersion",
+                table: "GameVersion",
+                column: "Id");
+        }
+    }
+}
